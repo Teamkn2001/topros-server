a6a9eef3a124a1be3b2f1e44515cdeb0ec89e2f7
SudtipongKn <166700563+Teamkn2001@users.noreply.github.com>
2025-01-15 20:43:57 +0700

    test: debug multer

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,16 +1,22 @@
-const path = require('path')
-const multer = require('multer')
-const os = require('os')  // Add this import
+const path = require("path");
+const multer = require("multer");
+const os = require("os");
+
+console.log("Temp directory:", os.tmpdir()); // Added this debug line
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, os.tmpdir())  // Use system temp directory instead of local path
-    },
-    filename: (req, file, cb) => {
-        const {id} = req.user
-        const fullFileName = `${id}_${Date.now()}_${Math.round(Math.random()*1000)}${path.extname(file.originalname)}`
-        cb(null, fullFileName)
-    }
-})
+  destination: (req, file, cb) => {
+    console.log("Saving file to:", os.tmpdir()); // Added another debug line
+    cb(null, os.tmpdir());
+  },
+  filename: (req, file, cb) => {
+    const { id } = req.user;
+    const fullFileName = `${id}_${Date.now()}_${Math.round(
+      Math.random() * 1000
+    )}${path.extname(file.originalname)}`;
+    console.log("Generated filename:", fullFileName); // Added debug line
+    cb(null, fullFileName);
+  },
+});
 
-module.exports = multer({storage: storage})
+module.exports = multer({ storage: storage });

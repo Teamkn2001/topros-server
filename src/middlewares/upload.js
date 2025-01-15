bc2c5745acb3882affc8d06eb3c06f94009c16b4
SudtipongKn <166700563+Teamkn2001@users.noreply.github.com>
2025-01-15 21:07:47 +0700

    fix: remove log at multer

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,11 +2,11 @@ const path = require("path");
 const multer = require("multer");
 const os = require("os");
 
-console.log("Temp directory:", os.tmpdir()); // Added this debug line
+// console.log("Temp directory:", os.tmpdir()); // Added this debug line
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log("Saving file to:", os.tmpdir()); // Added another debug line
+    // console.log("Saving file to:", os.tmpdir()); // Added another debug line
     cb(null, os.tmpdir());
   },
   filename: (req, file, cb) => {
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
     const fullFileName = `${id}_${Date.now()}_${Math.round(
       Math.random() * 1000
     )}${path.extname(file.originalname)}`;
-    console.log("Generated filename:", fullFileName); // Added debug line
+    // console.log("Generated filename:", fullFileName); // Added debug line
     cb(null, fullFileName);
   },
 });
